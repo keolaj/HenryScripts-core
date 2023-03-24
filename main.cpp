a0daf318020e5c0a6b4cbe57a0629ae6ddf2c14f
Keola <keolajarvegren@gmail.com>
2023-03-24 05:38:02 -1000

    figured out cmake include directory for ViGEm

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "src/headers/XInputHandler.h"
 
 int main() {
 	return 0;
---
+++ src/headers/XInputHandler.h
@@ -0,0 +1,12 @@
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#include <Xinput.h>
+
+#include <ViGEm/Client.h>
+
+class XInputHandler {
+	XInputHandler() {
+
+	}
+};
