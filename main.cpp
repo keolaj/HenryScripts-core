5f880dba18401895a1ab6b5d31d18a63d28de86f
Keola <keolajarvegren@gmail.com>
2023-03-27 19:22:26 -1000

    luajit working

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include "src/headers/XInputHandler.h"
 #include <sol/sol.hpp>
+#define SOL_ALL_SAFTIES_ON 1
 
 int main() {
 	sol::state lua{};
+	lua.open_libraries(sol::lib::base, sol::lib::jit);
+	lua.script("print(jit.status())");
+	return 0;
 }
